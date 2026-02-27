8540b0e42e8dc707366cb6002adc2a6d5d8342ed
brotherguns <brotherguns@users.noreply.github.com>
2026-02-27 00:14:07 +0000

    deploy: bb98986c8eaafd5b826a3ff32014c00108962f40

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-import { FluxDispatcher } from "@vendetta/metro/common";
-import { findByProps } from "@vendetta/metro";
+'use strict';
+
+var common = require('@vendetta/metro/common');
+var metro = require('@vendetta/metro');
 
 const MY_ID = "877502759404974110";
-
 const SCAM_IDS = new Set([
     "1476688857930924105",
     "1476688858375782701",
     "1476688858774110268",
-    "1476688859076104313",
+    "1476688859076104313"
 ]);
-
 const SCAM_HM = new Set([
     "d75ba302ba17fab02ad7a1de76e7282af8c86d6c7a17b0bfb5762dd67f7b9462",
     "21711a1fd5242d05e2ee36263b3b98e45423047a45e34ea7daecafed581ec76d",
     "6425e2a23c039ff644c62025b31716a3a5969621d7ee837fb39d4412dfd3dc77",
-    "26aec64263f89c79d71455f6e8c47ffe6d2180c6d006bfb7b9cba260699fbcab",
+    "26aec64263f89c79d71455f6e8c47ffe6d2180c6d006bfb7b9cba260699fbcab"
 ]);
-
 function extractAttachmentId(url) {
     if (typeof url !== "string") return null;
     var parts = url.split("/");
@@ -26,7 +25,6 @@ function extractAttachmentId(url) {
     if (!id) return null;
     return id.split("?")[0];
 }
-
 function extractHm(url) {
     if (typeof url !== "string") return null;
     var idx = url.indexOf("hm=");
@@ -35,34 +33,31 @@ function extractHm(url) {
     var end = val.indexOf("&");
     return end === -1 ? val : val.substring(0, end);
 }
-
 function isScamUrl(url) {
     return SCAM_IDS.has(extractAttachmentId(url)) || SCAM_HM.has(extractHm(url));
 }
-
 function isScamMessage(message) {
     var i, a, e, urls;
     var attachments = message.attachments || [];
-    for (i = 0; i < attachments.length; i++) {
+    for(i = 0; i < attachments.length; i++){
         a = attachments[i];
         if (isScamUrl(a.url) || isScamUrl(a.proxy_url)) return true;
     }
     var embeds = message.embeds || [];
-    for (i = 0; i < embeds.length; i++) {
+    for(i = 0; i < embeds.length; i++){
         e = embeds[i];
         urls = [
             e.image ? e.image.url : null,
             e.image ? e.image.proxy_url : null,
             e.thumbnail ? e.thumbnail.url : null,
-            e.thumbnail ? e.thumbnail.proxy_url : null,
+            e.thumbnail ? e.thumbnail.proxy_url : null
         ];
-        for (var j = 0; j < urls.length; j++) {
+        for(var j = 0; j < urls.length; j++){
             if (isScamUrl(urls[j])) return true;
         }
     }
     return false;
 }
-
 function onMessage(event) {
     try {
         var message = event && event.message;
@@ -71,18 +66,21 @@ function onMessage(event) {
         var channelId = event.channelId || message.channel_id;
         if (!authorId || !channelId || authorId === MY_ID) return;
         if (!isScamMessage(message)) return;
-        var MessageModule = findByProps("sendMessage", "editMessage");
-        MessageModule.sendMessage(channelId, { content: "?ban " + authorId });
+        var MessageModule = metro.findByProps("sendMessage", "editMessage");
+        MessageModule.sendMessage(channelId, {
+            content: "?ban " + authorId
+        });
     } catch (err) {
         console.error("[AutoBanScammer]", err);
     }
 }
-
-export default {
+var index = {
     onLoad: function() {
-        FluxDispatcher.subscribe("MESSAGE_CREATE", onMessage);
+        common.FluxDispatcher.subscribe("MESSAGE_CREATE", onMessage);
     },
     onUnload: function() {
-        FluxDispatcher.unsubscribe("MESSAGE_CREATE", onMessage);
-    },
+        common.FluxDispatcher.unsubscribe("MESSAGE_CREATE", onMessage);
+    }
 };
+
+module.exports = index;
